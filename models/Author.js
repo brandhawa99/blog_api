43cd529771095e73215307e3964b7b311d04fadb
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-18 10:59:55 -0700

    created author model

---
+++ models/Author.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name:{type:String, required:true, minlength:1},
+  last_name:{type:String, required:true, minlength:1},
+  username:{type:String, required:true, minlength:1}, 
+  password:{type:String, required:true},
+})
+
+module.exports = mongoose.model('AuthorSchema', AuthorSchema);
