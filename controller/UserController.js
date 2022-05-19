8eef89c59838d927819d03524d0b052c901924a3
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-19 08:49:25 -0700

    initialized controller for signing in and out

---
+++ controller/UserController.js
@@ -0,0 +1,16 @@
+const {body,validationResults,check} = require('express-validator');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+exports.user_signup_post = function(req,res,next){
+  res.send('need to implement');
+}
+
+exports.user_login_post = function(req,res,next){
+  res.send('need to implement');
+}
+
+// exports.user_
+
+
+
