646f4b561cab689fb50ed19b2bed4fc8c11e0449
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-19 08:48:58 -0700

    added paths for index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+const postController = require('../controller/PostController');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+//gets home page which gets the 5 most recent posts
+router.get('/',postController.index);
+//gets all the posts
+router.get('/posts', postController.posts_get);
+//gets a certain post and its comments
+router.get('/posts/:id',postController.get_post_by_id);
+
+
 
 module.exports = router;
