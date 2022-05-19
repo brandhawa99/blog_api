7e876f5232d10054f842d83c3174f17f0a0ddf60
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-19 08:48:46 -0700

    added a public boolean value

--- models/Post.js
+++ models/Post.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const {DateTime} = require('luxon');
-const { options } = require('../routes');
 
 const PostSchema = new Schema({
   author:{type:Schema.Types.ObjectId,ref:'Author',required:true},
   title:{type:String,maxlength:50, required:true},
   blog:{type:String,minlength:1, required:true},
-  timestamp:{type:Date,default: new Date().getTime()}
+  timestamp:{type:Date,default: Date.now()},
+  public:{type:Boolean, default:true},
 });
 
 PostSchema
