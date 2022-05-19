28fc8c9cd1ece3c647020de4a7b7aa751d90560f
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-19 08:49:36 -0700

    created paths for signing in and out

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controller/UserController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.post('/signup',userController.user_signup_post);
+
+router.post('/login', userController.user_login_post);
+
+
 
 module.exports = router;
