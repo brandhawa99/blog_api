0a8729928f42509f25b528beda097be2d6cffe37
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-19 08:49:41 -0700

    asdf

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+require('dotenv').config();
+
+
+//connect to database
+const mongodb = process.env.mongo_URI
+mongoose.connect(mongodb,{useUnifiedTopology:true,useNewUrlParser:true});
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'mongo connection error'));
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const User = require('./models/User');
 
 var app = express();
 
@@ -14,7 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/usr', usersRouter);
 
 module.exports = app;
