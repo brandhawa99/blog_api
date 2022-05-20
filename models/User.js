acc635d0be1ad06df2111c691e4383f1573d0dba
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-19 17:19:33 -0700

    nothing

--- models/User.js
+++ models/User.js
@@ -2,9 +2,9 @@ const { default: mongoose } = require("mongoose")
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username:{type:String,minlength:1,required:true},
+  username:{type:String, minlength:1, required:true},
   password:{type:String, required:true},
-  admin:{type:Boolean,default:false, required:true},
+  admin:{type:Boolean,default:false},
 })
 
 module.exports = mongoose.model('User',UserSchema);
