1eacc8058d2a046ed9e3736bee64b5ab26191af5
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-20 11:52:37 -0700

    added virtrual formated date

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const {DateTime} = require('luxon');
 
 const CommentSchema = new Schema({
   post:{type:Schema.Types.ObjectId,required:true},
@@ -7,5 +8,12 @@ const CommentSchema = new Schema({
   message:{type:String, required:true, minlength:1,maxlength:1500},
   timestamp:{type:Date, required:true, default:Date.now()}
 });
+
+CommentSchema
+.virtual('formated_date')
+.get(function(){
+  let date = DateTime.fromJSDate(this.timestamp).toLocaleDateString(DateTime.DATETIME_MED_WITH_SECONDS);
+  return date;
+})
 
 module.exports = mongoose.model('Comment',CommentSchema);
