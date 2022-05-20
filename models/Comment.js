22d52f03c5597e03ca04818b94486ea2c4301fa1
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-20 11:40:43 -0700

    changed the schema for comments

--- models/Comment.js
+++ models/Comment.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  user:{type:Schema.Types.ObjectId,required:true},
-  timestamp:{type:Date, required:true, default:Date.now()},
-  message:{type:String, required:true, minlength:1,maxlength:1500}
+  post:{type:Schema.Types.ObjectId,required:true},
+  name:{type:String, required:true, minlength:1},
+  message:{type:String, required:true, minlength:1,maxlength:1500},
+  timestamp:{type:Date, required:true, default:Date.now()}
 });
 
 module.exports = mongoose.model('Comment',CommentSchema);
