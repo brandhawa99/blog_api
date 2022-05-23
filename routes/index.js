9cc998202d4c7466302036f93d2509bd40f0cd80
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-22 23:20:43 -0700

    removed unused modules

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const postController = require('../controller/PostController');
-const userController = require('../controller/UserController')
+
 /* GET home page. */
 
 //gets home page which gets the 5 most recent posts
@@ -10,13 +10,6 @@ router.get('/',postController.index);
 router.get('/posts', postController.posts_get);
 //gets a certain post and its comments
 router.get('/posts/:id',postController.get_post_by_id);
-
-// router.post('/signup',userController.user_signup_post);
-
-// router.post('/login', userController.user_login_post);
-
-
-
 
 
 module.exports = router;
