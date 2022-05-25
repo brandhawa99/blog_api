65dee06629b02f11520fbd43e39a503c87952248
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-25 15:46:55 -0700

    moved logic to controller

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const AuthorController = require('../controller/AuthorConroller');
-const decoded = require('jwt-decode');
-const Author = require('../models/Author');
 
-router.get('/posts', function(req,res,next){
-  const Bearertoken = (req.headers.authorization);
-  const token = Bearertoken.split(' ')[1];
-  const crypt = decoded(token);
-  console.log(crypt.userid);
 
-  res.send({message:"You made it to the private route", token:crypt.userid})
-})
+
+router.get('/posts', AuthorController.get_posts);
 
 module.exports = router;
