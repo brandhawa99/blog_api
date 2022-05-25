4a70a39eeccaaf47cce6628489eb4447021f8e0d
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-24 23:15:28 -0700

    created private route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const AuthorController = require('../controller/AuthorConroller');
 
 
+router.get('/posts', function(req,res,next){
+  res.send('this is private')
+})
 
 module.exports = router;
