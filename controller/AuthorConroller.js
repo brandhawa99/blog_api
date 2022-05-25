1d021048234b0a774c02d82225a2412b0668d61e
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-25 15:46:34 -0700

    created function to simplify jwt decrypt

--- controller/AuthorConroller.js
+++ controller/AuthorConroller.js
@@ -1,8 +1,23 @@
 const {body,validationResult,check} = require('express-validator');
+const Author = require('../models/Author');
 const Post = require('../models/Post');
 const Comment = require('../models/Comment');
 const async = require('async');
+const decrypt = require('jwt-decode');
+
+
+//function used to decrypt token on requests 
+function getToken (Bearertoken) {
+  const token = Bearertoken.split(' ')[1];
+  const decrypted = decrypt(token)
+  return decrypted
+}
 
 exports.protected_get = function (req,res, next){
   return res.status(200).send('WE GOT TO THE PROTECTED ROUTE')
+}
+
+exports.get_posts = function(req,res,next){
+  const decryptedToken = getToken(req.headers.authorization);
+  res.send({message:"You made it to the private route", token:decryptedToken.userid})
 }
