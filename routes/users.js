3c17e74abf70290db8f2a5e191597edec5abf320
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-27 14:09:59 -0700

    created create post route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,6 @@ const AuthorController = require('../controller/AuthorConroller');
 
 router.get('/posts', AuthorController.get_posts);
 
+router.post('/posts/create', AuthorController.post_blog);
+
 module.exports = router;
