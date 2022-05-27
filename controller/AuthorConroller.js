7bf9bb8eac7f4fc1a7fc6bafc0fd4af9144260a6
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-27 14:08:50 -0700

    added comments

--- controller/AuthorConroller.js
+++ controller/AuthorConroller.js
@@ -12,12 +12,52 @@ function getToken (Bearertoken) {
   const decrypted = decrypt(token)
   return decrypted
 }
-
+/** 
+ * Test protected routes 
+ */
 exports.protected_get = function (req,res, next){
   return res.status(200).send('WE GOT TO THE PROTECTED ROUTE')
 }
 
+/**
+ * ---------------------------------------
+ * Get all the posts for the current user 
+ * ---------------------------------------
+ */
 exports.get_posts = function(req,res,next){
   const decryptedToken = getToken(req.headers.authorization);
-  res.send({message:"You made it to the private route", token:decryptedToken.userid})
+  Post.find({'author':decryptedToken.userid})
+    .sort({'timestamp' : -1})
+    .exec(function(err,posts){
+      if(err){
+        res.send({error:err})
+      }
+      res.send({posts:posts})
+    })
 }
+
+/**
+ * -----------------------------------------
+ * Post a new blog for the author to the db
+ * ------------------------------------------
+ */
+exports.post_blog = [
+  (req,res,next) =>{
+    const decryptedToken = getToken(req.headers.authorization);
+    const post = new Post ({
+      author: decryptedToken.userid,
+      title :req.body.title,
+      blog:req.body.blog, 
+      public :req.body.public
+    })
+
+    post.save(function(err){
+      if(err){
+        res.send({'error_message':err});
+        return next(err)
+        }
+      // res.redirect({postId : post._id})
+    })
+    res.send("POSTED")
+  }
+]
