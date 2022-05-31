5a5852f09ecdeb590de9a8787e07f0c50f43280b
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-05-30 21:48:00 -0700

    fixed typo

--- controller/PostController.js
+++ controller/PostController.js
@@ -1,11 +1,13 @@
 const {body,validationResult,check} = require('express-validator');
 const Post = require('../models/Post');
 const Comment = require('../models/Comment');
+const async = require('async');
 
 //Get the 5 most recent posts for the index page
 exports.index = function (req,res,next){
   Post.find({'public':true})
-    .sort({timestamp: -1})
+    .sort({"timestamp": -1})
+    .populate('author')
     .limit(5)
     .exec(function(err, results){
       if(err){
@@ -18,7 +20,9 @@ exports.index = function (req,res,next){
 
 //Gets all of the posts in the database
 exports.posts_get = function(req,res,next){
-  Post.find()
+  Post.find({'public':true})
+  .populate('author')
+  .sort({'timestamp':-1})
     .exec(function(err,results){
       if(err){return next(err)}
       res.send(results);
@@ -30,24 +34,25 @@ exports.get_post_by_id = function(req,res,next){
   async.parallel({
     Post: function(callback){
       Post.findById(req.params.id)
+        .populate('author')
         .exec(callback);
     },
     Comments: function(callback){
       Comment.find({'post':req.params.id})
       //order the comments where the newest ones are at the top
-        .sort({timestamp:-1})
+        .sort({'timestamp':-1})
         .exec(callback);
     }
   },function(err,results){
     if(err){next(err)}
-    res.send({post:results.Post,comments:results.Comments})
+    res.send({post:results.Post,date:results.Post.date,comments:results.Comments})
   })
 }
 
 // can POST comments at the bottom of the page
 exports.post_comment = [
   body('name').trim().isLength({min:1}).escape().withMessage('Name required'),
-  body('message').trim().isLength({min:1}).escape().withMessage('Message Required')
+  body('comment').trim().isLength({min:1}).escape().withMessage('Message Required')
     .isLength({max:750}).withMessage('Message is too long'),
 
   (req,res,next) =>{
@@ -55,7 +60,7 @@ exports.post_comment = [
     const comment = new Comment({
       post: req.params.id,
       name:req.body.name,
-      message: req.body.message,
+      message: req.body.comment,
     })
 
     if(!errors.isEmpty()){
