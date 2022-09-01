853c6f8b9227512d39afced4feace4cfd88dcb0f
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-01 15:19:40 -0700

    linting

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const passport = require("passport");
-const JWTStrategy = require("./strategies/jwt")
-const compression = require('compression');
-const helmet = require('helmet');
+const JWTStrategy = require("./strategies/jwt");
+const compression = require("compression");
+const helmet = require("helmet");
 //connect to database
 // const mongodb = process.env.mongo_URI
 // mongoose.connect(mongodb,{useUnifiedTopology:true,useNewUrlParser:true});
 // const db = mongoose.connection;
 // db.on('error',console.error.bind(console,'mongo connection error'));
 
-passport.use(JWTStrategy)
+passport.use(JWTStrategy);
 
-var indexRouter = require('./routes/index');
-var authorRouter = require('./routes/users');
-var authRouter = require('./routes/auth')
+var indexRouter = require("./routes/index");
+var authorRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(passport.initialize());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.options('*',cors());
+app.options("*", cors());
 app.use(compression());
 // app.use(helmet());
 app.use(cors());
+app.disable("x-powered-by");
 
-
-app.use('/', indexRouter);
-app.use('/auth',authRouter);
-app.use('/author',passport.authenticate('jwt',{session:false}),authorRouter);
-app.get('*', function(req, res){
-  res.status(404).send('what???');
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
+app.use(
+  "/author",
+  passport.authenticate("jwt", { session: false }),
+  authorRouter
+);
+app.get("*", function (req, res) {
+  res.status(404).send("what???");
 });
 
 // app.use(function(req,res,next){
