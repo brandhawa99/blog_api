25dc764e164f5b88d053e6da6fe39891e615404a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-01 15:20:07 -0700

    linting and async functions

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,102 +1,146 @@
-const {body,validationResult,check} = require('express-validator');
-const bcrypt = require('bcryptjs');
-const Author = require('../models/Author');
-const jwt = require('jsonwebtoken');
+const { body, validationResult, check } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const Author = require("../models/Author");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-
-//When a new author signs up 
+//When a new author signs up
 exports.author_signup_post = [
-  body('first_name').trim().escape().isLength({min:1}).withMessage("First name is required").isAlpha().withMessage('Last name must be only letters from the alphabet'),
-  body('last_name').trim().escape().isLength({min:1}).withMessage("Last name is required").isAlpha().withMessage('Last name must be only letters from the alphabet'),
-  body('username').trim().escape().isLength({min:1}).withMessage("Username is required"),
-  body('password').trim().escape().isLength({min:1}).withMessage("password is required"),
-  body('password2').trim().escape().isLength({min:1}).withMessage("password is required"),
-  check('password2','passwords do not match')
-  .custom((val,{req})=>{
-    if(val === req.body.password){
-      return true;
-    }else{
-      return false;
+  body("first_name")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("First name is required")
+    .isAlpha()
+    .withMessage("Last name must be only letters from the alphabet"),
+  body("last_name")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Last name is required")
+    .isAlpha()
+    .withMessage("Last name must be only letters from the alphabet"),
+  body("username")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Username is required"),
+  body("password")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("password is required"),
+  body("password2")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("password is required"),
+  check("password2", "passwords do not match").custom((val, { req }) => {
+    if (val === req.body.password) {
+      return Promise.resolve();
+    } else {
+      return Promise.reject();
     }
   }),
-  check('username','username already exists')
-  .custom(async(val)=>{
-    return Author.findOne({'username':val}).then(user =>{
-      if(user !== null){
-        return Promise.reject("username already in use")
+  check("username", "username already exists").custom(async (val) => {
+    try {
+      let author = await Author.findOne({ username: val }).exec();
+      if (author) {
+        throw new Error();
       }
-    })
+      return true;
+    } catch (error) {
+      return Promise.reject();
+    }
   }),
 
-  (req,res,next) => {
-
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-      return res.status(404).send({error:errors.array()})
-    }
-
-
-    bcrypt.hash(req.body.password, 10,(err,hashed) =>{
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      const hasErrors = !errors.isEmpty();
+      if (hasErrors) {
+        res.status(404).send({ error: errors.array() });
+        return;
+      }
+      const hashedPass = await bcrypt.hash(req.body.password, 10);
       const author = new Author({
-        first_name : req.body.first_name,
+        first_name: req.body.first_name,
         last_name: req.body.last_name,
         username: req.body.username,
-        password: hashed,
-      })
-      
-      author.save(function(err){
-        if(err){
-          res.status(400).send({error:[{msg:'There were some errors :('}]})
-          return next(err)
-        }
-        const id = author._id
-        const opts = {}
-        opts.expiresIn = 1000*60*60*24*5
-        const secret = process.env.SECRET;
-        const token = jwt.sign({"userid":id},secret,opts);
-        return res.send({success:true, token:"Bearer "+token})
-      })
-      if(err){
-        return res.status(400).send({error:[{msg:'There were some errors :('}]})        
-      }
-    })
-  }
-]
+        password: hashedPass,
+      });
 
-//When an existing author logs in 
-exports.author_login_post = [
-  body('username').trim().escape().isLength({min:1}).withMessage("Username is required"),
-  body('password').trim().escape().isLength({min:1}).withMessage('Password is required'),
-
-  (req,res,next)=>{
-    console.log(req.body);
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-      return res.status(404).send({error:errors.array()});
-      
-    }
-   
-    try {
-      Author.findOne({'username':req.body.username},function(err,user){
-        if(err){return res.status(404).send({error : [err]})}
-        if(!user){return res.status(404).json({error:[{msg:"username not found"}]})}
-        bcrypt.compare(req.body.password, user.password,(err,pass)=>{
-          const expiresIn = 1000*60*60*24*5+Date.now();
-          const opts = {}
-          opts.expiresIn = expiresIn;
-          const secret = process.env.SECRET;
-          const token = jwt.sign({"userid":user._id},secret,opts);
-          if(err){return res.status(404).send({error: [{msg:'Incorrect Password.'}]})}
-          if(pass){
-            return res.status(200).json({success:true, token: "Bearer "+token,expiresIn:expiresIn})
-          }
-        })
-      })  
+      let newAuthor = await author.save();
+      const id = newAuthor._id;
+      let opts = {};
+      opts.expiresIn = 1000 * 60 * 60 * 24 * 5 + Date.now();
+      const secret = process.env.SECRET;
+      const token = jwt.sign({ userid: id }, secret, opts);
+      res.status(200).send({ success: true, token: "Bearer " + token });
     } catch (error) {
-      res.status(404).json({error:'error'})
-      return next(err);
+      res.status(404).send({ msg: "there was an error signing up" });
     }
-  },  
-]
+  },
+];
 
+//When an existing author logs in
+exports.author_login_post = [
+  body("username")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Username is required"),
+  body("password")
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage("Password is required"),
+  check("username")
+    // TODO: ADD USERNAME AND PASSWORD CHECK
+    .custom(async (val, { req }) => {
+      try {
+        let user = await Author.findOne({ username: val }).exec();
+        if (user === null) {
+          throw new Error("username not found");
+        }
+        let passwordCheck = await bcrypt.compare(
+          req.body.password,
+          user.password
+        );
+        if (!passwordCheck) {
+          throw new Error(
+            "incorrect password " + user.password + req.body.password
+          );
+        }
+        return Promise.resolve();
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }),
+
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      const hasErrors = !errors.isEmpty();
+      if (hasErrors) {
+        res.status(400).send({ error: errors.array() });
+        return;
+      }
+      let author = await Author.findOne({ username: req.body.username }).exec();
+      let opts = {};
+      const expiresIn = 1000 * 60 * 60 * 24 * 5 + Date.now();
+      opts.expiresIn = expiresIn;
+      const secret = process.env.SECRET;
+      const token = jwt.sign({ userid: author._id }, secret, opts);
+      res
+        .status(200)
+        .send({
+          success: true,
+          token: "Bearer " + token,
+          expiresIn: expiresIn,
+        });
+    } catch (error) {
+      res.status(404).send(error);
+    }
+  },
+];
