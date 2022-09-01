4a4683814fabc79004a638badc50f808de4e8f2a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-01 15:21:20 -0700

    linting

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,98 +1,82 @@
-const request = require('supertest');
-const db = require('./db');
-const app = require('../app');
+const request = require("supertest");
+const db = require("./db");
+const app = require("../app");
 
-let author1, author2, post1,post2;
-
-beforeAll(async() =>{
+beforeAll(async () => {
   await db.setUp();
   await db.setupData();
-})
+});
 
-afterAll(async () =>{
+afterAll(async () => {
   await db.dropDatabase();
 });
 
 describe("Get Posts for api client", () => {
-  let res; 
-  test("GET /", async () =>{
-    res= await request(app)
-      .get('/')
-      .expect('Content-Type', /json/)
+  let res;
+  test("GET /", async () => {
+    res = await request(app)
+      .get("/")
+      .expect("Content-Type", /json/)
       .expect(200);
-      expect((res) =>{
-        res.body.length === 5; 
-        res.body[0].to.have.property('title');
-      res.body[0].to.have.property('author');
-      res.body[0].to.have.property('_id');
-      res.body[0].to.have.property('date');
-      res.body[0].to.have.property('url');
-      res.body[0].to.have.property('id');
-    })
-  })
+    expect((res) => {
+      res.body.length === 5;
+    });
+  });
 
-  test("GET /posts", async() => {
+  test("GET /posts", async () => {
     res = await request(app)
-    .get('/posts')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    expect((res)=>{
-        res.body.length >= 5;
-        res.body[0].to.have.property('blog');
-      })
-  })
+      .get("/posts")
+      .expect("Content-Type", /json/)
+      .expect(200);
+    expect((res) => {
+      res.body.length >= 5;
+      res.body[0].to.have.property("blog");
+    });
+  });
 
-  test("GET /post/:id", async () =>{
-    res = await request(app)
-      .get('/');
+  test("GET /post/:id", async () => {
+    res = await request(app).get("/");
     let id = res.body[0]._id;
 
-    res = await request(app)
-      .get(`/posts/${id}`)
-      .expect(200);
-      expect(res.body.post.author.first_name).toBe('name');
-      expect(res.body.post.author.last_name).toBe('name');
-      expect(res.body.post.author.username).toBe('user1');
-  })
+    res = await request(app).get(`/posts/${id}`).expect(200);
+    expect(res.body.post.author.first_name).toBe("name");
+    expect(res.body.post.author.last_name).toBe("name");
+    expect(res.body.post.author.username).toBe("user1");
+  });
 
-  test("GET /post/:id 'where id is random'", async() =>{
-    let res = await request(app)
-      .get('/posts/1230')
-      .expect(404);
-  })
-})
+  test("GET /post/:id 'where id is random'", async () => {
+    let res = await request(app).get("/posts/1230").expect(404);
+  });
+});
 
-describe("POST a add comment", () =>{
-
-  let post; 
-  let res
-  test("POST/", async () =>{
-    post = await request(app)
-      .get('/');
+describe("POST a add comment", () => {
+  let post;
+  let res;
+  test("POST/", async () => {
+    post = await request(app).get("/");
     let id = post.body[0]._id;
 
     res = await request(app)
       .post(`/posts/${id}`)
-      .set('Content-Type', 'application/json')
-      .send({name:"test_name", comment:"this is a test comment"})
+      .set("Content-Type", "application/json")
+      .send({ name: "test_name", comment: "this is a test comment" })
       .expect(200);
-    })
-  
-  test("POST/ to a short id", async () =>{
-    let res = await request(app)
-      .post('/posts/123123')
-      .set('Content-Type', 'application/json')
-      .send({name:"test", comment:"test comment"})
-      .expect(400);
-    expect(res.body.errors[0].msg).toBe('invalid');
-  })
-  test("POST/ to a random id", async () =>{
-    let res = await request(app)
-      .post('/posts/123123qwerqwe1234123412341234')
-      .set('Content-Type', 'application/json')
-      .send({name:"test", comment:"test comment"})
-      .expect(400);
-    expect(res.body.errors[0].msg).toBe('there is no post here');
-  })
+  });
 
-})
+  test("POST/ to a short id", async () => {
+    let res = await request(app)
+      .post("/posts/123123")
+      .set("Content-Type", "application/json")
+      .send({ name: "test", comment: "test comment" })
+      .expect(400);
+    expect(res.body.errors[0].msg).toBe("invalid");
+  });
+  test("POST/ to a random id", async () => {
+    let res = await request(app)
+      .post("/posts/123123qwerqwe1234123412341234")
+      .set("Content-Type", "application/json")
+      .send({ name: "test", comment: "test comment" })
+      .expect(400);
+    expect(res.body.errors[0].msg).toBe("there is no post here");
+  });
+});
