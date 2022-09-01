27824d735ad7d49de5529ac7a26ff61973424b02
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-09-01 15:20:54 -0700

    linting

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const AuthorController = require('../controller/AuthorConroller');
+const AuthorController = require("../controller/AuthorConroller");
 
+router.get("/posts", AuthorController.get_posts);
 
+router.post("/posts/create", AuthorController.post_blog);
 
-router.get('/posts', AuthorController.get_posts);
+router.get("/posts/:id", AuthorController.get_single_post);
 
-router.post('/posts/create', AuthorController.post_blog);
+router.post("/posts/update", AuthorController.update_blogpost);
 
-router.get('/posts/:id',AuthorController.get_single_post);
+router.post("/posts/:id/delete", AuthorController.delete_blogpost);
 
-router.post('/posts/update',AuthorController.update_blogpost)
-
-router.post('/posts/:id/delete',AuthorController.delete_blogpost);
-
-router.post('/comment/:id/delete',AuthorController.delete_comment);
+router.post("/comment/:id/delete", AuthorController.delete_comment);
 
 module.exports = router;
